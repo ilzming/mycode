ca739e4402cca3b3c74f3bfbcf6eaee0a5dd7240
ilzming <lzming1988@foxmail.com>
2016-11-07 18:29:24 +0800

    20161107

--- src/main/java/com/lzming/sort/InsertSort.java
+++ src/main/java/com/lzming/sort/InsertSort.java
@@ -2,6 +2,28 @@ package com.lzming.sort;
 
 /**
  * Created by liuzengming on 2016/11/7.
+ *
+ * 直接插入排序：
+ * 基本思想：在要排序的一组数中，假设前面(n-1)[n>=2]个数已经是排序好的
+ *
  */
 public class InsertSort {
+
+    /**
+     * 时间复杂度O(n^2)
+     */
+    public void insertSort(int[] a) {
+        //待插入元素
+        int temp;
+        //第一个当作已经排好序的
+        for (int i=1,len=a.length; i<len; i++) {
+            int j = i - 1;
+            temp = a[i];
+            //通过循环，逐个后移一位找到要插入的位置
+            for (; j>=0 && temp<a[j] ; j--) {
+                a[j+1] = a[j];
+            }
+            a[j+1] = temp; //插入
+        }
+    }
 }
---
+++ src/main/java/com/lzming/sort/ShellSort.java
@@ -0,0 +1,9 @@
+package com.lzming.sort;
+
+/**
+ * Created by liuzengming on 2016/11/7.
+ *
+ * 希尔排序（最小增量排序）
+ */
+public class ShellSort {
+}
